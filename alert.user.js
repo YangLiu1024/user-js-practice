c4bb579928d6d4f98d3a9d79dd56c6c0ebd2bcea
YangLiu1024 <31921184+YangLiu1024@users.noreply.github.com>
2023-04-19 19:29:49 +0800

    Update alert.user.js

--- alert.user.js
+++ alert.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       YangLiu
-// @match        https://github.com/YangLiu1024/*
+// @match        https://jira-apps-us.asml.com/browse/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=githubusercontent.com
 // @grant        none
 // ==/UserScript==

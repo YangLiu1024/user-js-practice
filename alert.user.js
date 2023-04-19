bd91e163abe8edaa5587b8ab1b432fc50eb13620
YangLiu1024 <31921184+YangLiu1024@users.noreply.github.com>
2023-04-19 18:06:18 +0800

    add first user.js

---
+++ alert.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         user js practice
+// @namespace    https://github.com/YangLiu1024
+// @version      0.1
+// @description  try to take over the world!
+// @author       YangLiu
+// @match        https://gist.githubusercontent.com/hampustagerud/190c29dd9eb3d63b7d8a920ffc349740/raw/8c7b06cf58a59a52170106b16b44043067257678/tmbbprh.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=githubusercontent.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    alert('hello world')
+    // Your code here...
+})();
